e6152ba95930c77abc21111b83cee38f0def584c
Lokman Jasari <ljasari88@gmail.com>
2026-02-01 22:52:24 +0100

    Week 02: Python fundamentals

---
+++ python-weekly-02/src/main.py
@@ -0,0 +1,24 @@
+# Week 2: Python fundamentals (Chapter 1) # project purpose
+ 
+whole_number = 10 # int: whole number (no decimal point)
+print("whole_number(starts):", whole_number)
+
+floating_number = 10.5
+print(floating_number)
+
+text = "Iam a String"
+print(text)
+
+
+whole_number = whole_number + 10
+print(whole_number)
+
+whole_number = whole_number * 2
+print(whole_number)
+
+
+price = 4.5
+
+price = price * 3
+print(price)
+
